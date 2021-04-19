450f9e497472333e4ac3b1e9c9a2b512775be460
Allan Nava <allannava95@gmail.com>
2021-04-19 18:30:00 +0200

    Update generate.sh

--- protos/generate.sh
+++ protos/generate.sh
@@ -5,5 +5,8 @@ set -o pipefail
 #
 #
 protoc -I. ./github.com/heroiclabs/nakama-common/api/api.proto --dart_out=grpc:../lib/src/generated
+#
+protoc -I. ./github.com/heroiclabs/nakama-common/api/realtime.proto --dart_out=grpc:../lib/src/generated
+#
 #protoc -I. ./apigrpc.proto --dart_out=grpc:lib/src/generated
 #

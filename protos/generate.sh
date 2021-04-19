49ce01300ee2d7ab5a23edd8ad9dcafcb257d460
allan <allan.nava@ies-italia.it>
2021-04-19 18:24:01 +0200

    updated lib generated src

---
+++ protos/generate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+#
+#
+protoc -I. ./github.com/heroiclabs/nakama-common/api/api.proto --dart_out=grpc:../lib/src/generated
+#protoc -I. ./apigrpc.proto --dart_out=grpc:lib/src/generated
+#
